2dc3f47b96fb65db5974dd54be1a707016703880
Han-wo <hansw98@naver.com>
2024-11-03 14:30:29 +0900

    refactor: 일부 레이아웃 변경

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -6,7 +6,7 @@ export default function SignUpPage() {
       <div className="flex w-full flex-col items-center justify-center p-4">
         <div className="mb-15 flex flex-col items-center justify-center gap-13">
           <div>로고</div>
-          <h1 className="text-16-600">회원가입후 서비스를 이용해주세요</h1>
+          <h1 className="text-18-700">회원가입후 서비스를 이용해주세요</h1>
         </div>
         <SignUpForm />
       </div>
