9fa203af412b02171642230b2807024b8ca97269
Han-wo <hansw98@naver.com>
2024-12-04 22:28:57 +0900

    feat: 로고에 링크걸기

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import LoginForm from "./login-form";
 
@@ -8,8 +9,17 @@ export default function LoginPage() {
       <div className="flex w-full flex-col items-center justify-center p-4">
         <div className="mb-15 flex flex-col items-center justify-center gap-13">
           <div className="flex w-250 gap-10">
-            <Image src="/icons/Logo.svg" alt="Logo" width={44} height={44} />
-            <span className="text-40-600">GrowFolio</span>
+            <Link href="/">
+              <div className="flex gap-10">
+                <Image
+                  src="/icons/Logo.svg"
+                  alt="Logo"
+                  width={44}
+                  height={44}
+                />
+                <span className="text-40-600">GrowFolio</span>
+              </div>
+            </Link>
           </div>
           <h1 className="mt-20 text-18-700">로그인후 서비스를 이용해주세요</h1>
         </div>
--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import SignUpForm from "./signup-form";
 
@@ -7,10 +8,12 @@ export default function SignUpPage() {
     <div className="bg-gray w-full min-w-400 shrink-0 rounded-20">
       <div className="flex w-full flex-col items-center justify-center p-4">
         <div className="mb-15 flex flex-col items-center justify-center gap-13">
-          <div className="flex w-250 gap-10">
-            <Image src="/icons/Logo.svg" alt="Logo" width={44} height={44} />
-            <span className="text-40-600">GrowFolio</span>
-          </div>
+          <Link href="/">
+            <div className="flex gap-10">
+              <Image src="/icons/Logo.svg" alt="Logo" width={44} height={44} />
+              <span className="text-40-600">GrowFolio</span>
+            </div>
+          </Link>
           <h1 className="text-18-700">회원가입후 서비스를 이용해주세요</h1>
         </div>
         <SignUpForm />
