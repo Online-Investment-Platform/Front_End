eb85161a150cdd7447ef2a4d020a05cdfdab08ce
Han-wo <hansw98@naver.com>
2024-12-06 00:37:48 +0900

    refactor: 로그인 회원가입 로고컴포넌트 분리

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,4 @@
-import Image from "next/image";
-import Link from "next/link";
+import Logo from "@/components/common/auth/logo";
 
 import LoginForm from "./login-form";
 
@@ -8,19 +7,7 @@ export default function LoginPage() {
     <div className="bg-gray w-full min-w-400 shrink-0 rounded-20">
       <div className="flex w-full flex-col items-center justify-center p-4">
         <div className="mb-15 flex flex-col items-center justify-center gap-13">
-          <div className="flex w-250 gap-10">
-            <Link href="/">
-              <div className="flex gap-10">
-                <Image
-                  src="/icons/Logo.svg"
-                  alt="Logo"
-                  width={44}
-                  height={44}
-                />
-                <span className="text-40-600">GrowFolio</span>
-              </div>
-            </Link>
-          </div>
+          <Logo />
           <h1 className="mt-20 text-18-700">로그인후 서비스를 이용해주세요</h1>
         </div>
         <LoginForm />
