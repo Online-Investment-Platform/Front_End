8a99bb24b30e60aba95809dfe7985a156f081977
Han-wo <hansw98@naver.com>
2024-12-08 18:08:42 +0900

    refactor: 메인 인풋창 보더 및 위치 수정

--- src/app/main/_components/search-stock.tsx
+++ src/app/main/_components/search-stock.tsx
@@ -20,7 +20,7 @@ export default function SearchStock() {
     <div className="mx-auto mb-30 flex items-center justify-center">
       <div className="w-full max-w-lg bg-white">
         <Input
-          className="!border-1  !h-32 !w-437 !rounded-80 !border-[#11E977] px-17 py-23 pl-20 hover:border-[#11E977] focus:border-[#11E977]"
+          className="!border-1 !h-32 !w-437 !rounded-80 !border-[#11E977] px-17 py-23 pl-20 hover:border-[#11E977] focus:border-[#11E977]"
           placeholder="종목명 또는 종목코드를 입력해주세요"
           onKeyDown={handleKeyDown}
         />
