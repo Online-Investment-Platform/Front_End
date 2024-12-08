f3946a908858b72d948da9cf839276c4fec09298
Han-wo <hansw98@naver.com>
2024-12-09 01:25:34 +0900

    fix: 맥북 픽셀 이슈 해결

--- src/app/search/[id]/_components/transaction-form/trade/buy-form-buttons.tsx
+++ src/app/search/[id]/_components/transaction-form/trade/buy-form-buttons.tsx
@@ -10,11 +10,11 @@ export default function BuyFormButtons({
   handleSubmit,
 }: BuyFormButtonsProps) {
   return (
-    <div className="relative top-90">
+    <div className="relative top-90 w-300">
       <Button variant="outline-gray" onClick={handleReset}>
         초기화
       </Button>
-      <Button variant="red" className="ml-7 w-160" onClick={handleSubmit}>
+      <Button variant="red" className="ml-8 w-160" onClick={handleSubmit}>
         매수
       </Button>
     </div>
