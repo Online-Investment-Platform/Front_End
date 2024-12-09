71eeab920f18aa726e0f6951a49df65ecd807ae9
Han-wo <hansw98@naver.com>
2024-12-09 15:31:58 +0900

    refactor: 거래폼 컴포넌트도 차트데이터에 맞게 수정

--- src/context/stock-info-context.tsx
+++ src/context/stock-info-context.tsx
@@ -1,15 +1,55 @@
-import { createContext, ReactNode, useContext } from "react";
+"use client";
 
+import { useQuery } from "@tanstack/react-query";
+import { createContext, ReactNode, useContext, useMemo } from "react";
+
+import { fetchStockInfo } from "@/api/candle-chart";
 import { StockInfo } from "@/app/search/[id]/types";
 
 interface StockInfoContextType {
   stockName: string;
   stockInfo: StockInfo;
+  isLoading: boolean;
 }
 
 const StockInfoContext = createContext<StockInfoContextType | undefined>(
   undefined,
 );
+
+interface StockInfoProviderProps {
+  children: ReactNode;
+  value: {
+    stockName: string;
+    stockInfo: StockInfo;
+  };
+}
+
+export function StockInfoProvider({
+  children,
+  value: { stockName, stockInfo: initialStockInfo },
+}: StockInfoProviderProps) {
+  const { data: stockInfo, isLoading } = useQuery({
+    queryKey: ["stockInfo", stockName],
+    queryFn: () => fetchStockInfo(stockName),
+    initialData: initialStockInfo,
+    // refetchInterval: 5000,
+  });
+
+  const contextValue = useMemo(
+    () => ({
+      stockName,
+      stockInfo: stockInfo ?? initialStockInfo,
+      isLoading,
+    }),
+    [stockName, stockInfo, initialStockInfo, isLoading],
+  );
+
+  return (
+    <StockInfoContext.Provider value={contextValue}>
+      {children}
+    </StockInfoContext.Provider>
+  );
+}
 
 export function useStockInfoContext(): StockInfoContextType {
   const context = useContext(StockInfoContext);
@@ -19,18 +59,4 @@ export function useStockInfoContext(): StockInfoContextType {
     );
   }
   return context;
-}
-
-export function StockInfoProvider({
-  children,
-  value,
-}: {
-  children: ReactNode;
-  value: StockInfoContextType;
-}) {
-  return (
-    <StockInfoContext.Provider value={value}>
-      {children}
-    </StockInfoContext.Provider>
-  );
 }
