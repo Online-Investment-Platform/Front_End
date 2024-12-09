c037a35f57a10775f9833ccb92b33daf2d79e1e5
Han-wo <hansw98@naver.com>
2024-12-10 02:44:38 +0900

    refactor: 거래폼 스키마 수정

--- src/validation/schema/transaction-form/index.tsx
+++ src/validation/schema/transaction-form/index.tsx
@@ -1,8 +1,19 @@
 import { z } from "zod";
 
 export const BuyFormSchema = z.object({
-  count: z.number().min(1, "수량은 1주 이상이어야 합니다"),
-  bidding: z.number().min(0, "호가는 0보다 커야 합니다"),
+  count: z
+    .number({
+      required_error: "수량을 입력해주세요",
+      invalid_type_error: "수량은 숫자여야 합니다",
+    })
+    .min(1, "수량은 1주 이상이어야 합니다"),
+
+  bidding: z
+    .number({
+      required_error: "호가를 입력해주세요",
+      invalid_type_error: "호가는 숫자여야 합니다",
+    })
+    .min(0, "호가는 0보다 커야 합니다"),
 });
 
 export type BuyFormData = z.infer<typeof BuyFormSchema>;
