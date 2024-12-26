1c7931c165dd6ecc7e68bcafe1d25012e38755b6
Han-wo <hansw98@naver.com>
2024-12-26 19:51:12 +0900

    merge: 86 브랜치와 머지지

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -6,10 +6,7 @@ export default function SignUpPage() {
   return (
     <div className="bg-gray w-full min-w-400 shrink-0 rounded-20">
       <div className="flex w-full flex-col items-center justify-center p-4">
-        <div className="mb-15 flex flex-col items-center justify-center gap-13">
-          <Logo />
-          <h1 className="text-18-700">회원가입후 서비스를 이용해주세요</h1>
-        </div>
+        <Logo />
         <SignUpForm />
       </div>
     </div>
